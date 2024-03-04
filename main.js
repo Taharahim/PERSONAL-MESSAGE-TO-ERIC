5e7834859f6ec027ba87a5004093754a40b2c0e7
TAHA RAHIM KHAN <160178509+Taharahim@users.noreply.github.com>
2024-03-04 20:36:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+let person_name = "Eric";
+console.log(`Hello ${person_name}, Would you like to some typescript today?`);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let person_name: string="Eric"
+
+console.log(`Hello ${person_name}, Would you like to some typescript today?`);
+
+ 
